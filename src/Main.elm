08f4223afa55f8da0756d9ae09863f8021bfe458
finch <blkfinch@gmail.com>
2018-12-13 14:31:57 -0700

    try hosting json locally to learn more about parsing

--- src/Main.elm
+++ src/Main.elm
@@ -1,32 +1,65 @@
 module Main exposing (..)
+import Http
+import Json.Decode as Decode
 import Browser
-import Html exposing (Html, text, div, h1, img)
-import Html.Attributes exposing (src)
+import Html exposing (..)
+import Html.Events exposing (..)
+import Html.Attributes exposing (class, style)
 
+---defining api calls
+randomCardUrl : String
+randomCardUrl =
+    "https://rws-cards-api.herokuapp.com/api/v1/cards/random"
 
 ---- MODEL ----
 
 
+-- type alias Card =
+--     { name : String
+--     , suit : String
+--     , desc : String
+--     }
+
 type alias Model =
-    {}
+    {
+        name : String
+    }
 
 
 init : ( Model, Cmd Msg )
 init =
-    ( {}, Cmd.none )
+    ( Model [], Cmd.none )
 
-
+---- REQUESTS/DECODERS ----
+cardDecoder : Decode.Decoder Model
+cardDecoder =
+    Decode.at ["cards"] Decode.string
 
 ---- UPDATE ----
-
+-- temp url to use
+url : String
+url =
+    "http://localhost:5019/nicknames"
 
 type Msg
-    = NoOp
-
+    = RequestCard
+    | DataRecieved(Result Http.Error String)
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-    ( model, Cmd.none )
+    case msg of
+        RequestCard ->
+           (model, Http.send DataRecieved (Http.getString url) )
+
+        DataRecieved (Ok cardJson) ->
+            case Decode.decodeString cardDecoder cardJson of
+                Ok card ->
+                    ({model | names = card}, Cmd.none )
+                Err errorMsg ->
+                    (Model [], Cmd.none)
+
+        DataRecieved(Err _) ->
+            (model, Cmd.none)
 
 
 
@@ -35,9 +68,15 @@ update msg model =
 
 view : Model -> Html Msg
 view model =
-    div [][
-      h1 [] [ text "Your Elm App is working!" ]
+    div [class "container"][
+      h1 [] [ text "Elm Tarot App" ]
+      ,button [class "btn btn-primary", onClick RequestCard][ text "Draw a Card!"]
+      ,p[](List.map nameList model.names)
     ]
+
+nameList : String -> Html Msg
+nameList name =
+    p[][text name]
 
 
 
